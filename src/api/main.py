67562a720a44a38517484aac80f125184cbeebab
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2023-08-14 01:12:19 +0000

    feat: add arquivo de padrões de formatação py
    Adicionado o arquivo pyproject.toml para padronizar a formatação do
    código python com o black formatter.

--- src/api/main.py
+++ src/api/main.py
@@ -1,14 +1,13 @@
 import os
+
 import openai
 from fastapi import FastAPI
-
-from Models.Resposta import Resposta
 from Models.Pergunta import Pergunta
+from Models.Resposta import Resposta
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
-
 
 
 @app.get("/")
@@ -18,20 +17,21 @@ async def root():
 
 @app.post("/chat-gpt-test")
 async def chat_gpt_test(Pergunta: str):
-    messages = [ {"role": "system", "content": "Você é um mestre de D&D 3.5, e você está mestrando uma aventura, aujude o usuário"} ]
+    messages = [
+        {
+            "role": "system",
+            "content": "Você é um mestre de D&D 3.5, e você está mestrando uma aventura, aujude o usuário",
+        }
+    ]
     # message = input("User : ")
     message = Pergunta
     if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
+        messages.append({"role": "user", "content": message})
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=messages
         )
     reply = chat.choices[0].message.content
     # print(f"ChatGPT: {reply}")
     # messages.append({"role": "assistant", "content": reply})
-    
-    return {
-        "resposta": reply
-    }
+
+    return {"resposta": reply}
